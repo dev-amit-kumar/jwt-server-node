33bb0dbfa16be94e104dba067375a633e5e3d495
Amit Kumar <amitkumar.developer1@gmail.com>
2021-02-20 16:25:28 +0530

    Added comments

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,11 @@ const loginValidation = require('../validations/loginValidation');
 
 // to add new user
 router.post('/register', registerValidation, userController.registerUser);
+
+// to login user
 router.post('/login', loginValidation, userController.loginUser);
+
+// to get the user details
 router.get('/profile', userController.userProfile);
 
 module.exports = router;
